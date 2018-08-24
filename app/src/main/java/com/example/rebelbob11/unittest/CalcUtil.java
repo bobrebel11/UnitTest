5d15d0eb927458e09e6a01f93b5111f5cb8de5a1
rebelbob11 <Hunter.11>
2018-08-24 09:50:59 -0400

    Initial commit

---
+++ app/src/main/java/com/example/rebelbob11/unittest/CalcUtil.java
@@ -0,0 +1,24 @@
+package com.example.rebelbob11.unittest;
+
+public class CalcUtil {
+
+    public static double Add(double x, double y){
+
+        return x + y;
+    }
+
+    public static double Subtract(double x, double y){
+
+        return x - y;
+    }
+
+    public static double Multiply(double x, double y){
+
+        return x * y;
+    }
+
+    public static double Divide(double x, double y){
+
+        return x / y;
+    }
+}
